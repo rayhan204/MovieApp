89dfa334b905639902f7965652e4a2b1162cdcc1
Rayhan <rgozali48@gmail.com>
2025-03-02 18:33:25 +0700

    back

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -25,13 +25,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
